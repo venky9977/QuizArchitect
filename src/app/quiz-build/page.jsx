0d522a372a71e2bef1281a5265b66d0899c34040
Prathamesh Dhake <prathameshdhake30@gmail.com>
2024-07-15 00:49:18 -0400

    Add the Support for Correct Answers on Quiz Build Page.

--- src/app/quiz-build/page.jsx
+++ src/app/quiz-build/page.jsx
@@ -1,19 +1,49 @@
 'use client';
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import QuizBuildNav from '../Components/QuizBuildPage/QuizBuildNav'
 import QuizBuildTitle from '../Components/QuizBuildPage/QuizBuildTitle'
 import QuizBuildQuestions from '../Components/QuizBuildPage/QuizBuildQuestions'
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from 'uuid';
 
 function Page(props){
+    
+    const [newQuiz, setNewQuiz] = useState({
+        id: uuidv4(),
+        icon: faCode, 
+        quizTitle: '',
+        quizQuestions: quizQuestions,
+    });
+
+    useEffect(() => {
+        setNewQuiz((prevQuiz) => ({
+            ...prevQuiz,
+            quizQuestions: quizQuestions,
+        }))
+    }, {quizQuestions});
+
     const [focusFirst, setFocusFirst] = useState(true);
+
+    function onChangeQuizTitle(text){
+        setNewQuiz((prevQuiz) => ({ ...prevQuiz, quizTitle, text }));
+    }
+    
+    const quizNavBarProps = {
+        quizQuestions,
+        newQuiz,
+    }
 
     const quizTitleProps = {
         focusProp: {focus: focusFirst, setFocusFirst},
+        onChangeQuizTitle,
     };
 
     const quizQuestionsProps = {
         focusProp: {focus: !focusFirst, setFocusFirst},
+        quizQuestionsProps,
+        setQuizQuestions,
     };
 
 
